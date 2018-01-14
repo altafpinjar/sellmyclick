9831ef97fc386113532f4a67a025ef8bbb6cd5e6
maktum <altafpinjar22@gmail.com>
2018-01-15 01:14:59 +0530

    Added imagesgrid page and changed navbar

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 
-<footer style="background-color:#191919;" class="footer">
+<footer style="background-color:#191919; font-size: 13px;" class="footer">
 
       <div class="container">
-  <div class="row">
+  <div class="row" style="padding-top: 10px;">
     <div class="col-6 col-sm-3">
-      <ul> <h4>Company</h4>
+      <ul><h6>Company</h6>
         <li>
           <a href="#"> About Us</a>
         </li>
@@ -23,7 +23,7 @@
       </ul>
     </div>
     <div class="col-6 col-sm-3">
-    <ul> <h4>Community</h4>
+    <ul> <h6>Community</h6>
         <li>
           <a href="#">Blogs</a>
         </li>
@@ -36,7 +36,7 @@
       </ul>
     </div>
     <div class="col-6 col-sm-3">
-<ul> <h4>Services</h4>
+<ul> <h6>Services</h6>
         <li>
           <a href="#">Edit</a>
         </li>
@@ -51,10 +51,10 @@
         </li>      
       </ul>
     </div>
-        <div class="col-6 col-sm-3"> <h4>Follow Us</h4>
-       <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-       <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
-       <i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i>
+        <div class="col-6 col-sm-3"> <h6>Follow Us</h6>
+       <a href="#"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i></a>
     </div>
   </div>
 </div>
--- header.php
+++ header.php
@@ -1,13 +1,13 @@
-
-<nav class="navbar navbar-expand-md navbar-light bg-dark" style="overflow: hidden; position: fixed; top: 0;">
-    <a href="/" class="navbar-brand text-white"  style="margin-left: 35px;">Sell My Click</a>
+<!--
+<nav class="navbar navbar-expand-md navbar-light bg-dark" style="position: fixed; top: 0;">
+    <a href="/" class="navbar-brand text-white">Sell My Click</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar5">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-collapse collapse" id="navbar5" >
+    <div class="navbar-collapse collapse" id="navbar5">
         <ul class="navbar-nav navbar-left" >
             <li class="nav-item dropdown" >
-        <a class="nav-link dropdown-toggle text-white"  href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+        <a class="nav-link dropdown-toggle text-white"  href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: 13px;" >
           Browse
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown" >
@@ -30,21 +30,72 @@
               </span>
             </div>
         </form>
-        <ul class="navbar-nav newClass navbar-right" style="padding-left: 45px;">
-            <li class="nav-item" style="padding-left: 35px;">
+        <ul class="navbar-nav newClass navbar-right" style="margin-left: 15px; font-family: 'Kavivanar', cursive; font-size: 13px;">
+            <li class="nav-item" ">
               <a class="nav-link " href="#">Editorial </a>
             </li>
-            <li class="nav-item" style="padding-left: 35px;">
+            <li class="nav-item" >
               <a class="nav-link" href="#">Upload</a>
             </li>
-            <li class="nav-item" style="padding-left: 35px;">
+            <li class="nav-item" >
               <a class="nav-link " href="#">Sell</a>
             </li>
-            <li class="nav-item" style="padding-left: 35px;">
-              <a class="nav-link" href="#">Login Or Sign Up  </a>
+            <li class="nav-item" >
+              <a class="nav-link" href="#">Login</a>
             </li>
         </ul>
     </div>
 </nav>
+-->
 
 
+
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand text-white" href="#">Sell My Click</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Category
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+     
+    </ul>
+      <form class="mx-2 my-auto d-inline w-50">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search here">
+                <span class="input-group-btn">
+                <button class="btn btn-outline-secondary" type="button">GO</button>
+              </span>
+            </div>
+        </form>
+    <ul class="navbar-nav newClass navbar-right" style="font-family: 'Kavivanar', cursive; font-size: 13px;">
+            <li class="nav-item" ">
+              <a class="nav-link " href="#">Editorial </a>
+            </li>
+            <li class="nav-item" >
+              <a class="nav-link" href="#"><i class="fa fa-upload" aria-hidden="true"></i>&nbsp;Upload</a>
+            </li>
+            <li class="nav-item" >
+
+              <a class="nav-link " href="#" ><i class="fa fa-sellsy" aria-hidden="true"></i>&nbsp;Sell</a>
+            </li>
+            <li class="nav-item" >
+              <a class="nav-link" href="#"><i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;Login or Sign Up</a>
+            </li>
+        </ul>
+  </div>
+</nav>
+
+-->
