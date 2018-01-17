02f772e5f55330efdef3f12497d56e80bea51490
sateesh <sateeshsourceus@gmail.com>
2018-01-17 10:01:18 -0500

    pull

--- buyer.php
+++ buyer.php
@@ -11,40 +11,23 @@ Date:        <dd-mm-yyyy>
 ?>
 
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
-    <?php include_once("stylesAndFont.php"); ?>
-
-
-</head>
-<body>
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!--styles css-->  
+    <?php include_once "styles.php"; ?>
+    <!--styles css--> 
+  </head>
+  <body style="background-color: #d3d3d3;">
+  <!--navbar-->
+  <?php include_once "header.php"; ?>
+  <!--end of navbar-->
   
-<!-- Mobile bar starts here -->
 
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <ul> <!-- User class="activeLink" to make links active -->
-        <li><a href="#">Home</a></li>
-        <li><a href="#">DCent MASS</a></li>
-        <li><a href="#">Our Services</a></li>
-        <li><a href="#">Student Hub</a></li>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Media</a></li>
-        <!-- <li><a href="#">Forum</a></li> -->
-        <li style="border-bottom:  2px solid #06f;"><a href="#">Contact Us</a></li>
-        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#">FAQs</a></li> -->
-  </ul>
-</div>
-
-<span id="openNavID" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
-<!-- Mobile bar ends here -->
-  
   <div class="container-fluid m-0 p-0 ">
   
   
