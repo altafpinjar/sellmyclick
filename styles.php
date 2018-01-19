c6319ebb4af73ea3f7fd30b7a5f21653ad41827a
sumaiyakounain <sumaiyakounainsourceus@gmail.com>
2018-01-19 14:28:09 +0530

    added css file

--- styles.php
+++ styles.php
@@ -1,4 +1,4 @@
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
  <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
  
  <link rel="stylesheet" type="text/css" href="css/stylep.css">
@@ -10,18 +10,11 @@
 
  <link rel="stylesheet" type="text/css" href="css/manju.css">
 
+ <link rel="stylesheet" type="text/css" href="css/sumaiya.css">
+
 
 <link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
-
-                       
-
-						   
-    <link rel="stylesheet" type="text/css" href="css/sateesh.css">						   
-						   
-
 <link href="https://fonts.googleapis.com/css?family=Saira+Condensed" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet">
-
 <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Raleway" rel="stylesheet">
-
 
